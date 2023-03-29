4cfa917a03dc7733a5bbef4b1b45c025058a4aba
Nestor <nestor-dp@hotmail.com>
2023-03-28 23:39:01 -0300

    fixed GPIO pin for motor configutation

--- src/littlebot_drivers/motor.c
+++ src/littlebot_drivers/motor.c
@@ -4,28 +4,32 @@
 void MotorConfigure(void){
   //Configure PWM
   SysCtlPWMClockSet(SYSCTL_PWMDIV_1);
+
   SysCtlPeripheralEnable(SYSCTL_PERIPH_PWM0);
-  SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOC);
-  GPIOPinConfigure(GPIO_PC4_M0PWM6);
-  GPIOPinConfigure(GPIO_PC5_M0PWM7);
-  GPIOPinTypePWM(GPIO_PORTC_BASE, GPIO_PIN_4);
-  GPIOPinTypePWM(GPIO_PORTC_BASE, GPIO_PIN_5);
-  PWMGenConfigure(PWM0_BASE, PWM_GEN_3, PWM_GEN_MODE_UP_DOWN | PWM_GEN_MODE_NO_SYNC);
-  PWMGenPeriodSet(PWM0_BASE, PWM_GEN_3, 25000);
-  PWMOutputState(PWM0_BASE, PWM_OUT_6_BIT | PWM_OUT_7_BIT, true);
-  PWMGenEnable(PWM0_BASE, PWM_GEN_3);
+  SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
+
+  GPIOPinConfigure(GPIO_PB4_M0PWM2);
+  GPIOPinConfigure(GPIO_PB5_M0PWM3);
+
+	GPIOPinTypePWM(GPIO_PORTB_BASE, GPIO_PIN_5);
+	GPIOPinTypePWM(GPIO_PORTB_BASE, GPIO_PIN_4);
+
+  PWMGenConfigure(PWM0_BASE, PWM_GEN_1, PWM_GEN_MODE_UP_DOWN | PWM_GEN_MODE_NO_SYNC);
+  PWMGenPeriodSet(PWM0_BASE, PWM_GEN_1, 25000);
+  PWMOutputState(PWM0_BASE, PWM_OUT_7_BIT | PWM_OUT_6_BIT, true);
+  PWMGenEnable(PWM0_BASE, PWM_GEN_1);
 
   // Configure GPIO
-  SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
-  while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOB)){}
-  SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD);
-  while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOD)){}
   SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);
   while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOE)){}
-  GPIOPinTypeGPIOOutput(GPIO_PORTB_BASE, GPIO_PIN_3);
-  GPIOPinTypeGPIOOutput(GPIO_PORTD_BASE, GPIO_PIN_2);
+
+  SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD);
+  while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOD)){}
+
+  GPIOPinTypeGPIOOutput(GPIO_PORTE_BASE, GPIO_PIN_2);
+  GPIOPinTypeGPIOOutput(GPIO_PORTE_BASE, GPIO_PIN_3);
   GPIOPinTypeGPIOOutput(GPIO_PORTD_BASE, GPIO_PIN_3);
-  GPIOPinTypeGPIOOutput(GPIO_PORTE_BASE, GPIO_PIN_1);
+  GPIOPinTypeGPIOOutput(GPIO_PORTD_BASE, GPIO_PIN_4);
 }
 
 uint8_t MotorVelocity(uint8_t side, uint16_t vel, uint8_t dir){
