d41e9209a6b6d485913150d5c34aa2976628d608
Nestor <nestor-dp@hotmail.com>
2023-05-19 00:03:39 -0300

    Created .h pid file

---
+++ include/littlebot_firmware/pid_controler.h
@@ -0,0 +1,52 @@
+// Copyright (c) 2022 Nestor D. Pereira Neto
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#ifndef INCLUDE_LITTLEBOT_FIRMWARE_PID_CONTROLER_H__
+#define INCLUDE_LITTLEBOT_FIRMWARE_PID_CONTROLER_H__
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+
+typedef struct PidControllerClass PidController;
+typedef struct Gain pid_gain;
+
+typedef float (*ptfPid)(PidController *self, floas set_point);
+
+//structure of function pointer
+struct PidControllerClass{
+  pidgain gain;
+  ptfPid PidController;
+  ptfWrite Write;
+  float t;
+};
+
+struct Gain{
+    float p;
+    float d;
+    float i;
+}
+
+void PidControllerContruct(PidController *self, float kp, float, kd, float ki);
+
+float fcPidController(PidController *self, floas set_point)
+
+
+#endif // INCLUDE_LITTLEBOT_FIRMWARE_PID_CONTROLER_H__
