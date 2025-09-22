0184cc9cb2ea7161ee407b336201eb4138e28c20
Nestor Neto <nestor-dp@hotmail.com>
2025-09-22 14:33:27 -0300

    🐛 fix(uart): Fix incorrect conditional in for loop

--- src/littlebot_drivers/uart_port.c
+++ src/littlebot_drivers/uart_port.c
@@ -60,7 +60,7 @@ uint32_t UartPortRead (char *msg) {
 uint32_t UartPortWrite (char *msg, size_t length) {
   uint32_t index;
   
-  for(index = 0; index == length; index++)
+  for(index = 0; index <= length; index++)
   {
       UARTCharPut(UART1_BASE, msg[index]);
   }
