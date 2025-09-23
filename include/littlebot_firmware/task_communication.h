b9589591f953e3e153a56dccf980585926add09e
Nestor Neto <nestor-dp@hotmail.com>
2025-09-23 16:40:00 -0300

    ✨ feat(communication task): Add nanopb functionalities

--- include/littlebot_firmware/task_communication.h
+++ include/littlebot_firmware/task_communication.h
@@ -6,6 +6,9 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+#include "pb_encode.h"
+#include "pb_decode.h"
+
 #include "inc/hw_memmap.h"
 #include "inc/hw_types.h"
 #include "driverlib/gpio.h"
@@ -19,6 +22,7 @@
 
 #include "littlebot_firmware/serial_wrapper.h"
 #include "littlebot_firmware/priorities.h"
+#include "littlebot_msg.pb.h"
 
 
 uint32_t CommunicationTaskInit(void);
--- src/task_communication.c
+++ src/task_communication.c
@@ -4,9 +4,8 @@
 
 #define SERIAL_READ_TASK_STACK_SIZE 128         // Stack size in words
 #define SERIAL_READ_TASK_DELAY      100
-
-
-extern SerialWrapper bluetooth;
+#define RIGHT                       1
+#define LEFT                        0
 
 // Command
 extern xQueueHandle g_pVelocityLeftQueue;
@@ -23,35 +22,66 @@ static void CommunicationTask(void *pvParameters) {
   portTickType ui32WakeTime;
   uint32_t ui32ReadDelay;
 
-  char rx_msg[80] = "\0";
-  char tx_msg[80] = "\0";
-  float velocity[2] = {0, 0};
-  float feed_back_velocity[2];
-  float feed_back_position[2];
+  static char rx_msg[100] = "\0";
+  static char tx_msg[100] = "\0";
+  float velocity[2] = {0.0, 0.0};
+  float feed_back_velocity[2] = {0.0, 0.0};
+  float feed_back_position[2] = {0.0, 0.0};
 
   ui32ReadDelay = SERIAL_READ_TASK_DELAY;
   ui32WakeTime = xTaskGetTickCount();
 
+  /* Resource to stablish the serial communication */
+  SerialWrapper bluetooth;
+  SerialWrapperConstructor(&bluetooth, 115200);
+
+  // Test nanopb
+  //========================================================================
+  bluetooth.Write(&bluetooth, "LittleBot Firmware");
+
+  littlebot_Wheels wheels = littlebot_Wheels_init_zero;
+  wheels.side_count = 2;
+  wheels.side[RIGHT].command_velocity = 1.0;
+  wheels.side[RIGHT].status_velocity = 2.0;
+  wheels.side[RIGHT].status_position = 2.0;
+  wheels.side[LEFT].command_velocity = 3.0;
+  wheels.side[LEFT].status_velocity = 4.0;
+  wheels.side[LEFT].status_position = 4.0;
+
+  pb_ostream_t stream = pb_ostream_from_buffer((uint8_t *)tx_msg, sizeof(tx_msg));
+  if (!pb_encode(&stream, littlebot_Wheels_fields, &wheels)) {
+      bluetooth.Write(&bluetooth, "Encoding failed");
+  } else {
+    static char hex_buffer[256];  // 2 chars per byte + null terminator
+    for(int i = 0; i < stream.bytes_written; i++) {
+        sprintf(&hex_buffer[i*2], "%02X", tx_msg[i]);
+    }
+    bluetooth.Write(&bluetooth, hex_buffer);
+  }
+
+  bluetooth.Write(&bluetooth, "ooooooooooooooooooooo");
+  //========================================================================
+
  
   while(1) {
-    // Read velocity status queue
+    /* Read velocity status queue */
     xQueueReceive(g_pFBVelocityLeftQueue, &feed_back_velocity[0], 0);
     xQueueReceive(g_pFBVelocityRightQueue, &feed_back_velocity[1], 0);
-    
-    // Read velocity status queue
+
+    /* Read position status queue */
     xQueueReceive(g_pFBPositionLeftQueue, &feed_back_position[0], 0);
     xQueueReceive(g_pFBPositionRightQueue, &feed_back_position[1], 0);
 
-    // Receive message from serial
-    // bluetooth.Read(&bluetooth, rx_msg);
+    /* Receive message from serial */
+    bluetooth.Read(&bluetooth, rx_msg);
 
     switch (rx_msg[0]) {
       case 'W':
-        // bluetooth.Write(&bluetooth, "1");
+        bluetooth.Write(&bluetooth, "1");
         rx_msg[0] = '\0';
       break;
       case 'R':
-        // bluetooth.Write(&bluetooth, tx_msg);
+        bluetooth.Write(&bluetooth, tx_msg);
         rx_msg[0] = '\0'; 
       break;
       default:
@@ -77,6 +107,6 @@ uint32_t CommunicationTaskInit(void) {
         return(1);
     }
 
-    // Success.
+    /* Success. */
     return(0);
 }
